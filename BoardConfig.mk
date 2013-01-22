65a38d98642533087a735ac9f45095874df1a575
Rob Shannon <spirit2457@gmail.com>
2013-01-22 03:45:57 -0700

    1st

---
+++ BoardConfig.mk
@@ -0,0 +1,82 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm7x30
+-include device/htc/msm7x30-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/htc/primou/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := primou
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=primou no_console_suspend=1
+BOARD_KERNEL_BASE := 0x14300000
+BOARD_KERNEL_PAGE_SIZE := 4096
+
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# cat /proc/emmc
+#dev:        size     erasesize name
+#mmcblk0p17: 00040000 00000200 "misc"
+#mmcblk0p21: 0087f400 00000200 "recovery"
+#mmcblk0p22: 00400000 00000200 "boot"
+#mmcblk0p25: 22dffe00 00000200 "system"
+#mmcblk0p27: 12bffe00 00000200 "cache"
+#mmcblk0p26: 496ffe00 00000200 "userdata"
+#mmcblk0p28: 014bfe00 00000200 "devlog"
+#mmcblk0p29: 00040000 00000200 "pdata"
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_primou_defconfig
+TARGET_PREBUILT_KERNEL := device/htc/primou/prebuilt/root/kernel
+
+# This is needed so CWM will function properly
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk0p32
+BOARD_USES_MMCUTILS := false
+BOARD_HAS_NO_MISC_PARTITION := false
+
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Actual UMS different from TWRP for some reason
+
+BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun0/file
+
+# Script
+
+TARGET_RECOVERY_INITRC := device/htc/primou/recovery/init-cwm.rc
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
---
+++ device.mk
@@ -0,0 +1,139 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# common msm7x30 configs
+$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/etc/gps.conf:system/etc/gps.conf
+
+# media
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    device/htc/primou/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/htc/primou/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/root/init.primou.rc:root/init.primou.rc \
+    device/htc/primou/prebuilt/root/init.usb.rc:root/init.usb.rc \
+    device/htc/primou/prebuilt/root/init.rc:root/init.rc \
+    device/htc/primou/prebuilt/root/ueventd.rc:root/ueventd.rc \
+    device/htc/primou/prebuilt/root/ueventd.primou.rc:root/ueventd.primou.rc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/primou/primou-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.3_r3 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    ro.sf.lcd_density=240
+
+# Override /proc/sys/vm/dirty_ratio on UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+	
+DEVICE_PACKAGE_OVERLAYS += device/htc/primou/overlay
+
+# GPS / Lights / Sensors
+PRODUCT_PACKAGES += \
+    gps.primou \
+    lights.primou \
+    sensors.primou
+
+# Additional packages
+PRODUCT_PACKAGES += \
+    Torch \
+    PinyinIME
+
+# idc files
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    device/htc/primou/prebuilt/usr/idc/primou-keypad.idc:system/usr/idc/primou-keypad.idc \
+    device/htc/primou/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+    device/htc/primou/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+    device/htc/primou/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/primou/prebuilt/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
+    device/htc/primou/prebuilt/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
+    device/htc/primou/prebuilt/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
+    device/htc/primou/prebuilt/usr/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm \
+    device/htc/primou/prebuilt/usr/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl \
+    device/htc/primou/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/htc/primou/prebuilt/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/primou/prebuilt/usr/keylayout/primou-keypad.kl:system/usr/keylayout/primou-keypad.kl \
+    device/htc/primou/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/primou/prebuilt/usr/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl
+
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+
+# init.d files
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/etc/init.d/01swap:system/etc/init.d/01swap
+
+# patched zram scripts
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/bin/compcache:system/bin/compcache \
+    device/htc/primou/prebuilt/bin/handle_compcache:system/bin/handle_compcache
+
+# bluez config
+PRODUCT_COPY_FILES += \
+    device/htc/primou/prebuilt/etc/dbus.conf:system/etc/dbus.conf \
+    device/htc/primou/prebuilt/etc/dbus-1/system-services/org.bluez.service:system/etc/dbus-1/system-services/org.bluez.service
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/primou/prebuilt/root/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_LOCALES += en_GB
+
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+## Recovery
+
+PRODUCT_COPY_FILES += \
+    device/htc/primou/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
+    device/htc/primou/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
+    device/htc/primou/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
+    device/htc/primou/recovery/sbin/power_test:recovery/root/sbin/power_test
+
+# HTC Audio
+$(call inherit-product, device/htc/primou/media_a1026.mk)
+$(call inherit-product, device/htc/primou/media_htcaudio.mk)
+
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
